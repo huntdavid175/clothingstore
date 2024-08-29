3bde0b1f898136ca9fb9877c2069835068cae8b9
huntdavid175 <huntdavid175@gmail.com>
2024-08-28 22:52:50 -0400

    added product name

--- src/app/shop/men/product/page.tsx
+++ src/app/shop/men/product/page.tsx
@@ -27,11 +27,19 @@ const ProductDisplay = () => {
         </div>
       </div>
       {/* main header ends  */}
+      {/* Carousel section  */}
       <div className="max-w-7xl">
         <div className="flex flex-col lg:flex-row">
           <ProductDisplayCarousel />
+          <div className="px-2">
+            <p className="text-3xl font-light text-gray-600">
+              The North Face Training 24 / 7 logo t-shirt in white
+            </p>
+            <p className="text-2xl font-semibold text-gray-500">$30.00</p>
+          </div>
         </div>
       </div>
+      {/* Carousel section ends here  */}
     </div>
   );
 };
