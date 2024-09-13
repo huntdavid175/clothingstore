f8cbee967c2b049075b8801c06d53f6de8f40714
huntdavid175 <huntdavid175@gmail.com>
2024-09-13 17:57:41 -0400

    edited privacy page

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -4,34 +4,35 @@ const PrivacyPolicyPage = () => {
   return (
     <div>
       <p>
-        Privacy Policy Effective Date: [Insert Date] Last Updated: [Insert Date]
-        At [Your Website Name], we are committed to protecting your privacy.
-        This Privacy Policy explains how we collect, use, disclose, and
-        safeguard your information when you visit our website [your website URL]
-        and/or use our services. By using our website, you consent to the data
-        practices described in this Privacy Policy. 1. Information We Collect We
-        may collect personal and non-personal information in various ways,
-        including: Personal Information: When you register on the platform,
-        subscribe, or contact us, we may collect personal details such as your
-        name, email address, phone number, and business details (Instagram
-        handle, etc.). Non-Personal Information: We may collect non-identifiable
-        information such as IP address, browser type, and website usage patterns
-        for analytics and improving user experience. 2. How We Use Your
-        Information We use the information we collect in the following ways: To
-        provide, operate, and maintain the website. To improve our services and
-        website functionality. To communicate with you, including customer
-        support, responding to your inquiries, and sending updates. To analyze
-        how users interact with the website and services. To personalize user
-        experiences and deliver relevant content. 3. Sharing Your Information We
-        will never sell or rent your personal information. However, we may share
-        your information with: Third-Party Service Providers: We may share data
-        with trusted third-party vendors who help us operate our website, such
-        as hosting, email, and analytics services. Legal Obligations: If
-        required by law, we may disclose your information to comply with legal
-        processes or governmental requests. 4. Cookies and Tracking Technologies
-        We may use cookies, web beacons, and other tracking technologies to
-        collect information and personalize your experience on our website. You
-        can set your browser to refuse cookies, but this may affect certain
+        Privacy Policy Effective Date: 12/02/2023 Last Updated: 12/08/2024 At
+        Online Boutique, we are committed to protecting your privacy. This
+        Privacy Policy explains how we collect, use, disclose, and safeguard
+        your information when you visit our website
+        https://clothingstore-amber.vercel.app and/or use our services. By using
+        our website, you consent to the data practices described in this Privacy
+        Policy. 1. Information We Collect We may collect personal and
+        non-personal information in various ways, including: Personal
+        Information: When you register on the platform, subscribe, or contact
+        us, we may collect personal details such as your name, email address,
+        phone number, and business details (Instagram handle, etc.).
+        Non-Personal Information: We may collect non-identifiable information
+        such as IP address, browser type, and website usage patterns for
+        analytics and improving user experience. 2. How We Use Your Information
+        We use the information we collect in the following ways: To provide,
+        operate, and maintain the website. To improve our services and website
+        functionality. To communicate with you, including customer support,
+        responding to your inquiries, and sending updates. To analyze how users
+        interact with the website and services. To personalize user experiences
+        and deliver relevant content. 3. Sharing Your Information We will never
+        sell or rent your personal information. However, we may share your
+        information with: Third-Party Service Providers: We may share data with
+        trusted third-party vendors who help us operate our website, such as
+        hosting, email, and analytics services. Legal Obligations: If required
+        by law, we may disclose your information to comply with legal processes
+        or governmental requests. 4. Cookies and Tracking Technologies We may
+        use cookies, web beacons, and other tracking technologies to collect
+        information and personalize your experience on our website. You can set
+        your browser to refuse cookies, but this may affect certain
         functionalities of the website. 5. Data Security We take appropriate
         measures to protect your personal information against unauthorized
         access, disclosure, alteration, or destruction. However, no online
@@ -52,8 +53,8 @@ const PrivacyPolicyPage = () => {
         Updated" date will be revised. Continued use of the website after
         changes are made will signify your acknowledgment and acceptance of the
         updated policy. 10. Contact Us If you have any questions or concerns
-        about this Privacy Policy, please contact us at: [Your Business Name]
-        Email: [Your Email Address] Phone: [Your Phone Number]
+        about this Privacy Policy, please contact us at: [Online Boutique]
+        Email: [don.falcone1996@gmail.com]
       </p>
     </div>
   );
