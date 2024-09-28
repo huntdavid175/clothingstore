2e950cdffde831a5c424e4b2c6cd783610f168f6
huntdavid175 <huntdavid175@gmail.com>
2024-09-27 21:10:14 -0400

    added side bar

--- src/components/Auth/Instagram/InstagramSignup.tsx
+++ src/components/Auth/Instagram/InstagramSignup.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 
 import { Instagram } from "lucide-react";
-import { signIn } from "../../../../auth";
 
 const InstagramSignup = () => {
   //   const handleInstagramLogin = async () => {
