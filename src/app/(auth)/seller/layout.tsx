f8ee0934a56d9455e36489421063a65448377daa
huntdavid175 <huntdavid175@gmail.com>
2024-10-01 14:36:13 -0400

    added layout to (auth)/seller/auth page

---
+++ src/app/(auth)/seller/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter, Raleway } from "next/font/google";
+import Navbar from "@/components/Nav/Navbar";
+import "../../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+const raleway = Raleway({ subsets: ["latin"], variable: "--font-raleway" });
+
+export const metadata: Metadata = {
+  title: "Online Boutique",
+  description:
+    "View all clothings being sold in Ghana and clothing sellers on instagram",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} ${raleway.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
