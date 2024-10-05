90bde61f3e7a379ae964aa106c978e0e1be1f833
huntdavid175 <huntdavid175@gmail.com>
2024-10-04 20:01:01 -0400

    updated confirm email route in seller folder

--- src/app/(auth)/seller/auth/confirm/route.ts
+++ src/app/(auth)/seller/auth/confirm/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const next = searchParams.get("next") ?? "/";
+  const next = searchParams.get("redirect_to") ?? "/";
 
   if (token_hash && type) {
     const supabase = createClient();
@@ -20,6 +20,7 @@ export async function GET(request: NextRequest) {
 
     if (!error) {
       // redirect user to specified redirect URL or root of app
+      console.log(next);
       return redirect(next);
     }
 
--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -21,7 +21,7 @@ export async function GET(request: NextRequest) {
     if (!error) {
       // redirect user to specified redirect URL or root of app
       console.log(next);
-      return redirect("https://clothingstore-amber.vercel.app/seller/admin");
+      return redirect("localhost:3000");
     }
 
     if (error.code === "otp_expired") {
