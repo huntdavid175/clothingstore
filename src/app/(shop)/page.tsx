1cdf1ce9a15186c445739f257e1e16b22c1a529b
huntdavid175 <huntdavid175@gmail.com>
2024-10-07 20:31:03 -0400

    functionality to save user choice in localstorage

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,25 +2,54 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [showPage, setShowPage] = useState<boolean>(false);
+
   const router = useRouter();
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center space-y-4 p-24">
-      <Button
-        className="w-[70%] lg:w-[50%] py-6"
-        onClick={() => router.push("/shop")}
-      >
-        Already Made
-      </Button>
+  const saveChoice = (choice: string) => {
+    localStorage.setItem("choice", choice);
+  };
 
-      <Button
-        className="w-[70%] lg:w-[50%] py-6"
-        onClick={() => router.push("/shop")}
-      >
-        Thrifts
-      </Button>
-    </main>
-  );
+  useEffect(() => {
+    const choice = localStorage.getItem("choice");
+    if (choice) {
+      router.push(`/shop/${choice}`);
+      return;
+    }
+    setShowPage(true);
+  }, []);
+
+  if (showPage) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center space-y-4 p-24">
+        <Button
+          className="w-[70%] lg:w-[50%] py-6 rounded-none"
+          onClick={() => {
+            saveChoice("men");
+            router.push("/shop/men");
+          }}
+        >
+          Men
+        </Button>
+        <Button
+          className="w-[70%] lg:w-[50%] py-6 rounded-none"
+          onClick={() => {
+            saveChoice("women");
+            router.push("/shop/women");
+          }}
+        >
+          Women
+        </Button>
+      </main>
+    );
+  } else {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center space-y-4 p-24">
+        <p className="text-3xl font-bold font-raleway">Online Boutique</p>
+      </main>
+    );
+  }
 }
