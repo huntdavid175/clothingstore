9f0ea4626efa6a90b79bca7147d6e381a321390b
huntdavid175 <huntdavid175@gmail.com>
2024-10-09 16:14:04 -0400

    added button on inventory page to addproduct page

--- src/app/(auth)/seller/admin/inventory/page.tsx
+++ src/app/(auth)/seller/admin/inventory/page.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
 import InventoryContent from "@/components/Admin/Seller/InventoryContent";
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { PlusIcon } from "lucide-react";
 
 const InventoryPage = () => {
   return (
     <div className="space-y-4">
-      <h2 className="text-2xl font-bold">Inventory</h2>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">Inventory</h2>
+        <div>
+          <Link href="/seller/admin/inventory/add-product">
+            <Button>
+              <PlusIcon className="mr-2 h-4 w-4" />
+              Add Product
+            </Button>
+          </Link>
+        </div>
+      </div>
       <InventoryContent />
     </div>
   );
